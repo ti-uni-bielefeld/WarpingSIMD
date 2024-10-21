49c6e7c99b3e8f89139f05a0a88efd3a0e74732c
Jonas Keller <jonas.r.keller@gmail.com>
2024-10-21 15:31:18 +0200

    Update format_cpp.sh

--- scripts/format_cpp.sh
+++ scripts/format_cpp.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env tcsh
+#!/usr/bin/env bash
 #===========================================================================
 #
-# formatAllHC --
-# run clang-format on all .H and .C files in the directory
+# runs clang-format on all .H and .C files in the given directory
 #
 # This file is part of the following software:
 # 
@@ -24,13 +23,30 @@
 #
 #===========================================================================
 
-foreach src (`ls -1 *.H *.C`)
-        # echo "formatting $src"
-        set tmpSrc = tmp_${src}
-        clang-format $src > $tmpSrc
-        if (`cmp $src $tmpSrc` != "") then
+if [ $# -ne 1 ]; then
+        echo "Usage: $0 <directory/file>"
+        exit 1
+fi
+
+if ! which clang-format > /dev/null; then
+        echo "Error: clang-format not found"
+        exit 1
+fi
+
+REQUIRED_CLANG_FORMAT_VERSION=16
+if [[ $(clang-format --version | grep -oP '(?<=version )\d+') -lt $REQUIRED_CLANG_FORMAT_VERSION ]]; then
+        echo "Error: clang-format version $REQUIRED_CLANG_FORMAT_VERSION or higher required (found $(clang-format --version))"
+        exit 1
+fi
+
+find $1 -type f \( -name "*.H" -o -name "*.C" \) | while read -r src; do
+        # "clang-format -i" can break links, so we format into a temporary file
+        # and then write the new content back
+        tmpSrc="${src}.tmp"
+        clang-format "$src" > "$tmpSrc"
+        if ! cmp -s "$src" "$tmpSrc"; then
+                cp -f "$tmpSrc" "$src"
                 echo "$src was re-formatted"
-        endif
-        /bin/cp -f $tmpSrc $src
-        /bin/rm -f $tmpSrc
-end
+        fi
+        rm -f "$tmpSrc"
+done
